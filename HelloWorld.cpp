85d1da72846363ce4e90c3b8a5f109b82495858e
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-08 23:09:47 +0530

    Print Hello World and Basic Syntax for Cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>// Header file for input and output functions
+
+using namespace std;
+
+// main function : where the execution of program begins
+int main(){
+    
+    cout << "Hello World..\n";// print Hello World
+
+    cout << "This is new item\n";// print the different string or statement 
+
+    cout << "Enter a Digit between 10 to 20\n";// print the different string or statement
+
+    // "\n" for new line 
+    return 0;
+}
