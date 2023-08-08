28b975bb6aa124217856cbe6bc91c19744794c31
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-08 23:56:46 +0530

    Print Hello World

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,13 @@
-//vThis is a preprocessor directive. The #include directive tells
+// This is a preprocessor directive. The #include directive tells
 // the compiler to include the content of a file in the source code.
+
 // For example, #include<iostream> tells the compiler to include
 // the standard iostream file which contains declarations of all
 // the standard input/output library functions.
+
+
+
+
 #include <iostream>// Header file for input and output functions
 
 using namespace std;
@@ -10,6 +15,7 @@ using namespace std;
 // main function : where the execution of program begins
 int main(){
     
+    // std::cout << "Hello World"; // use cout of std library
     cout << "Hello World..\n";// print Hello World
 
     cout << "This is new item\n";// print the different string or statement 
