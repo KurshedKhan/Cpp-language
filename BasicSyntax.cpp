c564a95b629c41f347029ad877f18d83400adf5a
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-09 06:37:30 +0530

    Basic Syntax of Cpp

---
+++ BasicSyntax.cpp
@@ -0,0 +1,15 @@
+#include <iostream>// Header File Library
+
+using namespace std;// std library
+
+int main(){// main function(Entry Point)
+
+    int number1 = 10;// Declaration of the variable
+    int number2 = 20;// Declaration of the variable
+
+    int sum = number1 + number2;
+
+    cout << "Total: " << sum << endl;// output
+
+    return 0;// Return Statement
+}
