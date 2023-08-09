5a88e141d783289532dd09f0734bd9254cf98893
Kurshed Khan <139382632+KurshedKhan@users.noreply.github.com>
2023-08-09 07:20:27 +0530

    3.Token

---
+++ Token.cpp
@@ -0,0 +1,59 @@
+// Single line Comment
+/*
+    Multiple line Comment
+*/
+
+/*
+        Tokens in C
+                    Tokens are the smallest individual units in a program.
+
+                    Type of the Tokens
+
+                    1.Keywords
+                    2.Identifiers
+                    3.Constants
+                    4.Strings
+                    5.Operators
+                    6.Special Symbols
+
+                    1.Keywords : The keywords are pre-defined or reserved words in a programming language.
+
+                    auto         double      int        struct
+                    break        else        long       switch
+                    case         enum        register   typedef
+                    char         extern      return     union
+                    const        float       short      unsigned
+                    continue     for         signed     void
+                    default      goto        sizeof     volatile
+                    do           if          static     while
+
+
+                    2. Identifiers are used  the naming of variables, functions, and arrays.
+
+                       1. They must begin with a letter or underscore(_).
+                       2. They must consist of only letters, digits, or underscore.
+                       3. No other special character is allowed.
+                       4. It should not be a keyword.
+                       5. It must not contain white space.
+                       
+                       Note: Identifiers are case-sensitive so names like variable and Variable
+                        will be treated as different.
+
+                    3. Constants : The constants refer to the variables with fixed values.\
+
+                        exp. : const int Const_variable = 20;// This value is fixed..
+
+                    6. Special Symbol : 
+                                        Brackets[],
+                                        Parentheses(),
+                                        Braces{},
+                                        Comma (, ),
+                                        Colon(:),
+                                        Semicolon(;),
+                                        Asterisk (*)
+                                        Assignment operator(=)
+                                        Pre-processor (#)
+                                        Period (.)
+                                        Tilde(~)
+                    
+*/
